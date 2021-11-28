d63f7a122bdbb25a6974431769bbd18249e9e48b
Adam <am1macdonald@gmail.com>
2021-11-28 06:02:30 -0500

    Website layout generated; starting to build modules for content creation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("Ouch that Yog-hurts...")
+import './reset.css';
+import './style.css';
+import image from './nick-karvounis-Ciqxn7FE4vE-unsplash.jpg';
+
+console.log("Ouch that Yog-hurts...");
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 const path = require('path');
 
 module.exports = {
+    module: {
+        rules: [
+            {   test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
     mode: 'development',
     entry: {
         index: './src/index.js',
