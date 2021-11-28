1d2fbfcd85f1be3ae4546b99aa92c9d66d312574
Adam <am1macdonald@gmail.com>
2021-11-28 07:27:00 -0500

    Added hero to pageLoad module

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,20 +1,20 @@
 function pageLoad(){
 
-    const page = document.getElementById("content");
+    const page = document.getElementById('content');
     const nav = document.createElement('nav');
     const pageTitle = document.createElement('h1');
     const navLinksDiv = document.createElement('div');
     const navBtn = document.createElement('button');
     const navBtnNames = ['Menu', 'Contact', 'Home'];
-
-
-
-    
+    const main = document.createElement('main');
+    const imgDiv = document.createElement('div');
+    const backgroundImg = document.createElement('IMG');
+    const footer = document.createElement('footer');
+    const heroDiv = document.createElement('div');
+    const hero = document.createElement('div');
     pageTitle.innerHTML = 'Restaurant de Lierre';
+
     navLinksDiv.id = 'nav-links';
-
-
-
     nav.appendChild(pageTitle);
     nav.appendChild(navLinksDiv);
     for (let i = 0; i < navBtnNames.length; i++) {
@@ -25,10 +25,30 @@ function pageLoad(){
         newBtn.id = `${navBtnNames[i]}-btn`.toLowerCase();
         navLinksDiv.appendChild(newBtn);
     };
+
+    imgDiv.id = 'img-div';
+    backgroundImg.setAttribute('src', "..\\src\\nick-karvounis-Ciqxn7FE4vE-unsplash.jpg");
+    backgroundImg.setAttribute('alt', "Une image d'un bon restaurant / A nice restaurant picture.");
+    imgDiv.appendChild(backgroundImg);
+    main.appendChild(imgDiv);
+
+
+
+    heroDiv.id = 'hero-div';
+    hero.id = 'hero';
+
+    heroDiv.appendChild(hero);
+
+    main.appendChild(heroDiv);
+
     page.appendChild(nav);
+    page.appendChild(main);
+    page.appendChild(footer);
+    
 
 
     /*
+
     < nav >
         <H1>Restaurant de Lierre</H1>
         <div id="nav-links">
@@ -41,6 +61,13 @@ function pageLoad(){
         <div id="img-div">
                 <img src="..\src\nick-karvounis-Ciqxn7FE4vE-unsplash.jpg" alt="Une image d'un bon restaurant/A nice restaurant picture.">
         </div>
+
+
+
+    // UP TO HERE
+
+
+
         <div id="hero-div">
             <div id="hero">
 
