7a879456756dae282a8809b35d57f46e7a5af377
Adam <am1macdonald@gmail.com>
2021-12-04 07:19:45 -0500

    housekeeping

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,21 +1,23 @@
 function pageLoad(){
 
+    //  sets up the cache of page elements
     const page = document.getElementById('content');
     const nav = document.createElement('nav');
     const pageTitle = document.createElement('h1');
     const navLinksDiv = document.createElement('div');
     const navBtn = document.createElement('button');
-    const navBtnNames = ['Home', 'Menu', 'Contact'];
     const main = document.createElement('main');
     const imgDiv = document.createElement('div');
     const backgroundImg = document.createElement('IMG');
     const footer = document.createElement('footer');
 
     pageTitle.innerHTML = 'Restaurant de Lierre';
-
     navLinksDiv.id = 'nav-links';
     nav.appendChild(pageTitle);
     nav.appendChild(navLinksDiv);
+
+    //  renders the buttons for the nav bar
+    const navBtnNames = ['Home', 'Menu', 'Contact'];
     for (let i = 0; i < navBtnNames.length; i++) {
         let newBtn = navBtn.cloneNode();
         newBtn.innerHTML = navBtnNames[i];
@@ -25,22 +27,22 @@ function pageLoad(){
         navLinksDiv.appendChild(newBtn);
     };
 
+    //  renders the background image to the page
     imgDiv.id = 'img-div';
     backgroundImg.setAttribute('src', "./4110222ed96379e04bc1.jpg");
     backgroundImg.setAttribute('alt', "Une image d'un bon restaurant / A nice restaurant picture.");
     imgDiv.appendChild(backgroundImg);
     page.appendChild(imgDiv);
 
-
+    
+    //  renders the footer elements
     const makeFooter = function() {
-
         const footerList = document.createElement('ul');
         const footerLink = document.createElement('a');
         const footerItem = document.createElement('li');
         const footerLinks = {
             'Github' : 'https://github.com/am1macdonald'
         };
-
         for (let link in footerLinks) {
             let newItem = footerItem.cloneNode();
             let newLink = footerLink.cloneNode();
@@ -48,8 +50,7 @@ function pageLoad(){
             newLink.setAttribute('href', `${footerLinks[link]}`);
             newItem.appendChild(newLink);
             footerList.appendChild(newItem);
-        }
-        
+        }        
         footer.appendChild(footerList);
     }
 
@@ -65,6 +66,14 @@ function pageLoad(){
             parent.removeChild(parent.firstChild);
         }
     }
+    function todayMonth() {
+        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+            'August', 'September', 'October', 'November', 'December'];
+        const date = new Date();
+        return months[date.getMonth()];
+    }
+    
+    //  button event controller
     function launch(e) {
         let btn = e.target.id;
         if (btn == 'menu-btn') {
@@ -78,29 +87,19 @@ function pageLoad(){
         };
     }
     function renderHome() {
-
         clearDoc(main);
-
         const heroDiv = document.createElement('div');
         const hero = document.createElement('div');
         const heroCopy = document.createElement('p');
-
         heroDiv.classList.add('content-div');
         hero.id = 'hero';
-
         heroCopy.innerText = 'New York\'s finest french dining experience.'
-
-
         hero.appendChild(heroCopy);
-
         heroDiv.appendChild(hero);
-
         main.appendChild(heroDiv);
     }
     function renderMenu() {
-
         clearDoc(main);
-
         const menuDiv = document.createElement('div');
         const menu = document.createElement('div');
         const menuHeader = document.createElement('h3');
@@ -114,8 +113,7 @@ function pageLoad(){
             'Duck Pâté en Croûte' : 'wild duck and porcini mushroom',
             'Boeuf Bourguignon': 'grass-fed beef in burgundy',
             'Café Crème Brûlée': 'rich cream & single origin coffee'
-        };       
-
+        };
         menuDiv.classList.add('content-div');
         menu.id = 'menu';
         listDiv.id = 'menu-item-div';
@@ -123,7 +121,6 @@ function pageLoad(){
         menuHeader.innerHTML = `${todayMonth()}`;
         menuPrice.innerHTML = '$200';
         menuFooter.innerHTML = '*Please inquire about our wine list'
-
         for (let key in menuItemsObj) {
             const itemName = document.createElement('h4');
             itemName.classList.add('menu-item-title');
@@ -131,35 +128,22 @@ function pageLoad(){
             itemDescription.classList.add('menu-item-description');
             itemName.innerHTML = `${key}`;
             itemDescription.innerHTML = `${menuItemsObj[key]}`;
-
             const newMenuItem = menuItem.cloneNode();
             newMenuItem.classList.add('menu-item');
             newMenuItem.appendChild(itemName);
             newMenuItem.appendChild(itemDescription);
-            menuList.appendChild(newMenuItem);
-            
+            menuList.appendChild(newMenuItem);            
         };
-
         menu.appendChild(menuHeader);
         listDiv.appendChild(menuList);
         listDiv.appendChild(menuPrice);
         menu.appendChild(listDiv);
         menu.appendChild(menuFooter);
-
         menuDiv.appendChild(menu);
-
         main.appendChild(menuDiv);
-    }
-    function todayMonth() {
-
-        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-         'August', 'September', 'October', 'November', 'December'];
-        const date = new Date();
-        return months[date.getMonth()];
     }
     function renderContact() {
         clearDoc(main);
-
         const contactDiv = document.createElement('div');
         const contactInfo = document.createElement('div');
         const contactHeader = document.createElement('h3');
@@ -179,26 +163,17 @@ function pageLoad(){
         for (let att in mapAttributes){
             map.setAttribute(`${att}`, `${mapAttributes[att]}`);
         }
-
         contactHeader.innerHTML = 'Contact us';
-
         for (let contact in contactsObject) {
             let newContact = contactItem.cloneNode();
             newContact.innerHTML = `${contact}: ${contactsObject[contact]}`;
             contactList.appendChild(newContact);
         };
-
         contactDiv.classList.add('content-div');
         contactInfo.id = 'contact-info';
-
-        
-
-
         contactInfo.appendChild(contactHeader);
         contactInfo.appendChild(contactList);
         contactInfo.appendChild(map);
-
-
         contactDiv.appendChild(contactInfo);
         main.appendChild(contactDiv);
     }
